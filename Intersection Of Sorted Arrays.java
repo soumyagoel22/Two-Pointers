fb4c2bebdb4e9fb7f962961d1877ba6b89493011
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-06-16 22:26:57 +0530

    Create Intersection Of Sorted Arrays.java

---
+++ Intersection Of Sorted Arrays.java
@@ -0,0 +1,29 @@
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public ArrayList<Integer> intersect(final List<Integer> A, final List<Integer> B) {
+        int i,j,n,m;
+        i=0;
+        j=0;
+        n=A.size();
+        m=B.size();
+        ArrayList<Integer> C=new ArrayList<Integer>();
+        while(i<n && j<m)
+        {
+            if(A.get(i)<B.get(j))
+            {
+                i++;
+            }
+            else if(A.get(i)>B.get(j))
+            {
+                j++;
+            }
+            else
+            {
+                C.add(A.get(i));
+                i++;
+                j++;
+            }
+        }
+        return C;
+    }
+}
