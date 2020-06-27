91bfe40a9a1c44cdb747ae966084d895bcb86d3f
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-06-27 14:12:00 +0530

    Create 3 Sum.java

---
+++ 3 Sum.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int threeSumClosest(ArrayList<Integer> A, int B) {
+         int res = 0, diff = Integer.MAX_VALUE;
+        Collections.sort(A);
+        for(int i = 0; i < A.size() - 2; i++) {
+            int j = i + 1, k = A.size() - 1;
+            while(j < k) {
+                int sum = A.get(i) + A.get(j) + A.get(k);
+                
+                if(sum == B) return sum;
+                else if(sum > B) k--;
+                else j++;
+                
+                if(Math.abs(B - sum) < diff) {
+                    res = sum;
+                    diff = Math.abs(B - sum);
+                }
+                
+            }
+        }
+        
+        return res;
+    }
+}
